2aefd3955f3e54c9d34d7535a1b9cdc66ebdf29d
YT <guoyanting0919@gmail.com>
2021-01-22 18:13:34 +0800

    nuxt vuetify tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = ({ dev, rootDir, srcDir }) => ({
+  theme: {},
+  variants: {},
+  plugins: [],
+  purge: [
+    // "./components/**/*.{vue,js}",
+    // "./layouts/**/*.vue",
+    // "./pages/**/*.vue",
+    // "./plugins/**/*.{js,ts}",
+    // "./nuxt.config.{js,ts}"
+  ]
+});
